bf0bd54f821d252774c14d2bcdebc3d081b71098
Anna Kurachenko <anna.kurachenko3@gmail.com>
2024-11-11 22:53:00 +0200

    homework_4

---
+++ H_w_4.js
@@ -0,0 +1,28 @@
+function checkProbabilityTheory(count) {
+    let evenCount = 0; // лічильник парних чисел
+    let oddCount = 0;  // лічильник непарних чисел
+
+    for (let i = 0; i < count; i++) {
+        // генерація випадкового числа від 100 до 1000 бібліотекою Math
+        const rndNum = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+
+        // перевіряємо число на парність
+        if (rndNum % 2 === 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+    }
+
+    // відсоток парних чисел до непарних чисел через використання тернарного оператора
+    const evenToOdd = oddCount > 0 ? ((evenCount / oddCount) * 100).toFixed(2) : '100%';
+
+    // Виводимо результати
+    console.log("Кількість згенерованих чисел:", count);
+    console.log("Парних чисел:", evenCount);
+    console.log("Непарних чисел:", oddCount);
+    console.log(`Відсоток парних до непарних: ${evenToOdd}%`);
+}
+
+// Приклад виклику функції
+checkProbabilityTheory(100);

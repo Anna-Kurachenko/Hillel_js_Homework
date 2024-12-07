e90af9b0537f41cf57c4556338cd10665de9e1c1
Anna Kurachenko <anna.kurachenko3@gmail.com>
2024-12-07 18:32:35 +0200

    update H_W

---
+++ H_W_10.1.js
@@ -0,0 +1,35 @@
+var arr = [
+    {
+        userName: "Test",
+        lastName: "Test",
+        email: "test.test@gmail.com"
+    },
+    {
+        userName: "Dmitro",
+        lastName: "Porohov",
+        email: "dmitro.porohov@yahoo.com"
+    },
+    {
+        userName: "Andrii",
+        lastName: "",
+        email: "andrii@mail.ru" // Нам такі не підходять
+    },
+];
+
+// масив для перевірених email
+var trustedEmails = [];
+
+var emailRegex = /^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)?@(gmail\.com|yahoo\.com)$/;
+
+// перебираємо кожен об'єкт в масиві
+for (var i = 0; i < arr.length; i++) {
+    var email = arr[i].email;
+
+    // перевіряємо, чи email має домен gmail.com або yahoo.com
+    if (emailRegex.test(email)) {
+        trustedEmails.push(email);
+    }
+}
+
+// виводимо масив масив з адресами, гідними довіри
+console.log(trustedEmails);
